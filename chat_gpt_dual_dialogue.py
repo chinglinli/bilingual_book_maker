f72683660cbf782b0410a7da990facc8499a4516
Chinglin Li <chinglin.li@gmail.com>
2023-04-06 20:42:49 +0800

    ee

--- chat_gpt_dual_dialogue.py
+++ chat_gpt_dual_dialogue.py
@@ -1,48 +1,37 @@
 ﻿
-# -*- coding: utf-8 -*-
 import openai
-import os
+from os import environ as env
 
+openai.api_key = env.get("OPENAI_API_KEY")
 
-
-# 使用您的 OpenAI API 密鑰進行身份驗證
-#openai.api_key = os.getenv("OPENAI_API_KEY")  # 將您的 API 密鑰設置為環境變量，或者直接在這裡填寫
-openai.api_key =""
-def chat_gpt_response(prompt):
+def generate_text(prompt):
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=prompt,
-        temperature=0.5,
-        max_tokens=100,
+        max_tokens=150,
         n=1,
         stop=None,
-        return_prompt=True,
+        temperature=0.5,
     )
     return response.choices[0].text.strip()
 
-def chat_gpt_conversation(start_prompt, rounds=5, summary_round=None):
-    conversation_history = start_prompt
-    for i in range(rounds):
-        gpt1_message = chat_gpt_response(conversation_history)
-        conversation_history += f"\nGPT-1: {gpt1_message}"
+# 對話歷史
+conversation_history = ""
 
-        gpt2_message = chat_gpt_response(conversation_history)
-        conversation_history += f"\nGPT-2: {gpt2_message}"
+# GPT-1 提問
+question1 = "如何提高供應鏈效率？"
+conversation_history += f"User: {question1}\n"
+answer1 = generate_text(conversation_history)
+conversation_history += f"GPT-1: {answer1}\n"
 
-        if summary_round is not None and i == summary_round - 1:
-            summary_prompt = "cc"
-            summary = chat_gpt_response(summary_prompt)
-            break
+# GPT-2 提問
+question2 = "實現供應鏈透明度的好方法是什麼？"
+conversation_history += f"User: {question2}\n"
+answer2 = generate_text(conversation_history)
+conversation_history += f"GPT-2: {answer2}\n"
 
-    return conversation_history, summary if summary_round is not None else None
-
-if __name__ == "__main__":
-    start_prompt = "please set up topic"
-    rounds = 5
-    summary_round = 5
-    conversation_history, summary = chat_gpt_conversation(start_prompt, rounds, summary_round)
-
-    print("history")
-    print(conversation_history)
-    print("\n Sumary")
-    print(summary)
+# 請求摘要
+summary_prompt = f"根據前面的對話，請總結關於提高供應鏈效率的 why、how 和 what：\n{conversation_history}"
+summary = generate_text(summary_prompt)
+print(summary)
+input()
